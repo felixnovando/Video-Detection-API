44a445bc178197e6051a84051ef0a6929764e5a3
FeliX novando <felixnovando@gmail.com>
2025-07-22 21:52:08 +0700

    init basic api

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("Hello world")
+import dotenv from "dotenv";
+import express from "express";
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(express.json());
+
+app.get("/", (_, res) => {
+    res.send("Success");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
